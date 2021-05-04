9585176a89ac6ad4d7019f0ae6291e917afd0e9b
antoine65 <antoine.lcts@gmail.com>
2021-05-04 14:35:31 +0200

    début de l'application

---
+++ src/comps/Title.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Title = () => {
+  return (
+    <div className="title">
+      <h1>Gallery Project</h1>
+      <h2>Vos Photos</h2>
+      <p>Partagez vos Photos ici ...</p>
+    </div>
+  )
+}
+
+export default Title;
