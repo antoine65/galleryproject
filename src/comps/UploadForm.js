1642d700542a5548a41e561511dac1f167e53e5f
antoine65 <antoine.lcts@gmail.com>
2021-05-04 15:30:02 +0200

    Configuration format image/erreur mauvais format

--- src/comps/UploadForm.js
+++ src/comps/UploadForm.js
@@ -1,10 +1,33 @@
 import React from 'react';
+import { useState } from 'react';
 
 const UploadForm = () => {
+    const [file, setFile] = useState(null);
+    const [error, setError] = useState(null);
+
+
+    const types = ['image/png', 'image/jpeg'];
+
+    const changeHandler = (e) => {
+        let selected = e.target.files[0];
+       
+        if(selected && types.includes(selected.type)){
+            setFile(selected);
+            setError('');
+        } else {
+            setFile(null);
+            setError ('Inserez une image en format png ou jpeg.')
+        }
+    }
+
 
 return (
 <form>
-    <input type ="file"/>
+    <input type ="file" onChange= {changeHandler}/>
+    <div className ="output">
+        { error && <div className = "error">{ error }</div> }
+        { file && <div> { file.name } </div>}
+    </div>
 
 </form>
 
