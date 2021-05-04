0236818da3a265d90157187ea11c31a06450d2eb
antoine65 <antoine.lcts@gmail.com>
2021-05-04 15:11:14 +0200

    Installation firebase/upload

---
+++ src/firebase/config.js
@@ -0,0 +1,23 @@
+ 
+ 
+ import * as firebase from 'firebase/app';
+ import 'firebase/storage';
+ import 'firebase/firestore';
+ 
+ // Your web app's Firebase configuration
+ var firebaseConfig = {
+    apiKey: "AIzaSyDPStftLPHXcp1FoHMZPqv7mhxP6qC5aDA",
+    authDomain: "galleryproject-444ba.firebaseapp.com",
+    projectId: "galleryproject-444ba",
+    storageBucket: "galleryproject-444ba.appspot.com",
+    messagingSenderId: "591466862698",
+    appId: "1:591466862698:web:95e8d9136402c6dc4f1213"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+ const projectStorage = firebase.storage();
+ const projectFireStore = firebase.firestore();
+
+
+ export { projectStorage , projectFireStore} ;
