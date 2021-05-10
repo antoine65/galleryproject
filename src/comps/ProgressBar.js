b0b6a34da14d3f6217c11482e67f085abfbf1929
antoine65 <antoine.lcts@gmail.com>
2021-05-10 22:34:54 +0200

    rectification des erreurs et remise en forme du code

--- src/comps/ProgressBar.js
+++ src/comps/ProgressBar.js
@@ -1,23 +1,22 @@
 import React, { useEffect } from 'react';
 import useStorage from '../hooks/useStorage';
-import {motion} from 'framer-motion';
+import { motion } from 'framer-motion';
 
 const ProgressBar = ({ file, setFile }) => {
-    const {url, progress} = useStorage(file);
-    
-    useEffect(()  => {
-        if(url){
-            setFile(null);
-        }
-    }, [url, setFile])
+  const { progress, url } = useStorage(file);
 
-    return (
-        <motion.div className ="progress-bar">
-            initial={{width:0 }}
-            animate={{ width: progress + '%'}}
+  useEffect(() => {
+    if (url) {
+      setFile(null);
+    }
+  }, [url, setFile]);
 
-        </motion.div>
-    )
-}
+  return (
+    <motion.div className="progress-bar"
+      initial={{ width: 0 }}
+      animate={{ width: progress + '%' }}
+    ></motion.div>
+  );
+} 
 
 export default ProgressBar;
